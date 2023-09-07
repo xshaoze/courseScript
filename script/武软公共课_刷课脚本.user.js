42d04b23e96b0e854fa7384b39d4376c50d2604d
少泽 <87309647+xshaoze@users.noreply.github.com>
2023-09-07 19:08:29 +0800

    Create 武软公共课_刷课脚本.user.js

---
+++ script/武软公共课_刷课脚本.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         武软公共课_刷课脚本
+// @namespace    wrggk.mtail.cn
+// @version      0.1
+// @description  武软公共课_刷课脚本
+// @author       ze
+// @match        *://wrggk.whvcse.edu.cn/*
+// @icon         https://wrggk.whvcse.edu.cn/Web/Templates/img/ico/favicon.png
+// @grant        none
+// @updateURL    
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
