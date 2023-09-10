66e1d8d1ea707e5ee9282096ba480363d0410553
少泽 <87309647+xshaoze@users.noreply.github.com>
2023-09-10 12:27:29 +0800

    Update 武软公共课_刷课脚本.user.js

--- script/武软公共课_刷课脚本.user.js
+++ script/武软公共课_刷课脚本.user.js
@@ -6,12 +6,12 @@
 // @author       ze
 // @match        *://wrggk.whvcse.edu.cn/*
 // @icon         https://wrggk.whvcse.edu.cn/Web/Templates/img/ico/favicon.png
-// @grant        none
-// @updateURL    
+// @grant        GM_addStyle
+// @updateURL    https://coursescript.mtail.cn/script/%E6%AD%A6%E8%BD%AF%E5%85%AC%E5%85%B1%E8%AF%BE_%E5%88%B7%E8%AF%BE%E8%84%9A%E6%9C%AC.user.js
+// @require      file://E:\MystilTail\ClassScript\武软公开课\武软公共课_刷课脚本.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-
     // Your code here...
 })();
